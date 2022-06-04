7f78a2d417bc4c20c1eaed3f6c6946270c95fa3a
FabioKrakauer <comvocefabio@gmail.com>
2022-06-04 17:58:48 -0300

    add navigation to question page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,17 @@
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import './Home.css';
 
 function Home() {
+    const navigate = useNavigate();
     return (
         <div className="container">
             <div className="wrapper">
                 <h1 className="text1">Que <label htmlFor="" className="text2">planta</label></h1>
                 <h1 className="text3">eu sou?</h1>
             </div>
-            <button className="button">começar</button>
+            <button className="button" onClick={() => navigate('/question')}>começar</button>
         </div >
     )
 }
 
 export default Home;
-
-//onClick={() => console.log("You clicked on the pink circle!")}
